c7c8643b9083db3e872865236274699f82ab6f3c
Vishal-Borse <vishalsborse8@gmail.com>
2023-10-02 19:52:53 +0530

    add on

--- app.js
+++ app.js
@@ -2,32 +2,34 @@ const express = require("express");
 const axios = require("axios");
 const _ = require("lodash");
 const app = express();
+require("dotenv").config();
+app.use(express.static("public"));
+app.use(express.json());
 
-// TODO: .env would help here
-/*
-  Create a readme file for steps and provide a .env.sample file for reference if possible.
-  Also, if not feasible, add a top level comment in the code showing why a .env based approach is not taken.
-*/
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+const API_URL = "https://intent-kit-16.hasura.app/api/rest/blogs";
 
 // Define a memoization cache for analytics
 const blogsAnalyticsCache = _.memoize(
   async () => {
+    const startTime = new Date(); // Record the start time
     try {
       //Fetching blog data from the third-party API
       const response = await axios.get(
-        "https://intent-kit-16.hasura.app/api/rest/blogs", // TODO: code this into a constant variable at the top
+        API_URL, // TODO: code this into a constant variable at the top
         {
           headers: {
-            "x-hasura-admin-secret":
-              "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6", // TODO: .env please
+            "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET, // TODO: .env please
           },
         }
       );
 
       // Check if the response status is not successful
       if (response.status !== 200) {
-        throw new Error(`Failed to fetch data from the third-party API. Status: ${response.status}`); 
+        throw new Error(
+          `Failed to fetch data from the third-party API. Status: ${response.status}`
+        );
       }
 
       // Perform data analysis using Lodash
@@ -40,10 +42,14 @@ const blogsAnalyticsCache = _.memoize(
       const uniqueTitles = _.uniqBy(blogs, "title");
 
       const statistics = {
-        totalBlogs,
-        longestBlog: longestBlog.title,
-        blogsWithPrivacy: blogsWithPrivacy.length,
-        uniqueTitles: uniqueTitles.map((blog) => blog.title),
+        lastCachedAt: startTime.toLocaleTimeString(), // Add last caching timestamp
+        timeToServeRequest: new Date() - startTime, // Calculate time to serve the request
+        blogStatistics: {
+          totalBlogs,
+          longestBlog: longestBlog.title,
+          blogsWithPrivacy: blogsWithPrivacy.length,
+          uniqueTitles: uniqueTitles.map((blog) => blog.title),
+        },
       };
 
       return statistics;
@@ -57,14 +63,14 @@ const blogsAnalyticsCache = _.memoize(
 // Define a memoization cache for search results
 const blogSearchCache = _.memoize(
   async (query) => {
+    const startTime = new Date(); // Record the start time
     try {
       // Fetch blog data from the third-party API
       const response = await axios.get(
-        "https://intent-kit-16.hasura.app/api/rest/blogs", // TODO: Same as above
+        API_URL, // TODO: Same as above
         {
           headers: {
-            "x-hasura-admin-secret":
-              "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6", // TODO: Same as above
+            "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET, // TODO: Same as above
           },
         }
       );
@@ -75,21 +81,31 @@ const blogSearchCache = _.memoize(
       Example - instead of just outputting failed to fetch the api, go more granular and send the exact root cause, just as an enhancement
       */
       if (response.status !== 200) {
-        throw new Error(`Failed to fetch data from the third-party API. Status: ${response.status}`); 
+        throw new Error(
+          `Failed to fetch data from the third-party API. Status: ${response.status}`
+        );
       }
 
       // Extract the blogs data from the response
-      const blogs = response.data["blogs"]; //TODO: Safe access here. Handle if the key is not present
+      const blogs = response.data?.blogs || []; //TODO: Safe access here. Handle if the key is not present
 
-      // Convert query to lowercase for case-insensitive search
-      query = query.toLowerCase(); // TODO: Same here, handle query not present
-
-      // Implement a search functionality
-      const searchResults = _.filter(blogs, (blog) =>
-        _.includes(blog.title.toLowerCase(), query)
-      );
+      let results;
+      if (query) {
+        query = query.toLowerCase();
+        results = _.filter(blogs, (blog) =>
+          _.includes(blog.title.toLowerCase(), query)
+        );
+      } else {
+        results = [];
+      }
+      const searchResults = {
+        lastCachedAt: startTime.toLocaleTimeString(), // Add last caching timestamp
+        timeToServeRequest: new Date() - startTime, // Calculate time to serve the request
+        SearchResults: results,
+      };
 
       return searchResults; // TODO: What if this is empty? Appropriate message if specified in the requirements?
+      //if searchResults is empty then we are sending empty array
     } catch (error) {
       throw error; // Rethrow the error to be handled later
     }
@@ -97,21 +113,46 @@ const blogSearchCache = _.memoize(
   (query) => query
 ); // Cache results based on the query string
 
-
 // TODO: Create a get route on / as well, so index route doesn't throw a 404
+app.get("/", (req, res) => {
+  res.send("Welcome to the SubSpace Assignment");
+});
 
 // Middleware to fetch blog analytics data
 // !TODO: This is not a middleware function
 app.get("/api/blog-stats", async (req, res) => {
   try {
+    const startTime = new Date(); // Record the start time
     const statistics = await blogsAnalyticsCache(); // Use memoized function
+    const timeToServeRequest = new Date() - startTime; // Calculate time to serve the request
+    statistics.timeToServeRequest = timeToServeRequest; // Add time to serve request to the response
     res.json(statistics);
   } catch (error) {
     // TODO: What kind of errors are we dealing with here?
     // TODO: Return messages based on the type of errors
     // Handle errors
-    console.error("Error:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    if (error.response) {
+      // Handle errors related to the HTTP response (e.g., 404, 401)
+      const status = error.response.status;
+      if (status === 404) {
+        res.status(404).json({ error: "API not found" });
+      } else if (status === 401) {
+        res
+          .status(401)
+          .json({ error: "Unauthorized. Check your credentials." });
+      } else {
+        res
+          .status(status)
+          .json({ error: `API request failed with status ${status}` });
+      }
+    } else if (error.message.includes("timeout")) {
+      // Handle timeout errors
+      res.status(504).json({ error: "Request timed out" });
+    } else {
+      // Handle other types of errors
+      console.error("Error:", error.message);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 });
 
@@ -119,14 +160,36 @@ app.get("/api/blog-stats", async (req, res) => {
 // !TODO: Same as above
 app.get("/api/blog-search", async (req, res) => {
   try {
+    const startTime = new Date(); // Record the start time
     const query = req.query.query;
     const searchResults = await blogSearchCache(query); // Use memoized function
+    const timeToServeRequest = new Date() - startTime; // Calculate time to serve the request
+    searchResults.timeToServeRequest = timeToServeRequest; // Add time to serve request to the response
     res.json(searchResults);
   } catch (error) {
     // TODO: Same as above
-    // Handle errors
-    console.error("Error:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    if (error.response) {
+      // Handle errors related to the HTTP response (e.g., 404, 401)
+      const status = error.response.status;
+      if (status === 404) {
+        res.status(404).json({ error: "API not found" });
+      } else if (status === 401) {
+        res
+          .status(401)
+          .json({ error: "Unauthorized. Check your credentials." });
+      } else {
+        res
+          .status(status)
+          .json({ error: `API request failed with status ${status}` });
+      }
+    } else if (error.message.includes("timeout")) {
+      // Handle timeout errors
+      res.status(504).json({ error: "Request timed out" });
+    } else {
+      // Handle other types of errors
+      console.error("Error:", error.message);
+      res.status(500).json({ error: "Internal server error" });
+    }
   }
 });
 
